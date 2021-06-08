2ae76596703438db300207aaa4c5e37e2e573f57
Dmitry Kitsenko <kitsenko@greensight.ru>
2021-06-08 21:44:41 +0300

    move constants to files and read from them

--- src/constants.js
+++ src/constants.js
@@ -4,31 +4,9 @@ const USER_ARGV_INDEX = 2;
 const DEFAULT_USER_COMAND = `--help`;
 const DEFAULT_COUNT = 1;
 const MAX_DESCRIPTION_LENGTH = 5;
+const DATA_DIR = `data`;
 
 const FILE_NAME = `mocks.json`;
-
-const TITLES = [
-  `Продам книги Стивена Кинга`,
-  `Продам новую приставку Sony Playstation 5`,
-  `Продам отличную подборку фильмов на VHS`,
-  `Куплю антиквариат`,
-  `Куплю породистого кота`,
-];
-
-const SENTENCES = [
-  `Товар в отличном состоянии.`,
-  `Пользовались бережно и только по большим праздникам.`,
-  `Продаю с болью в сердце...`,
-  `Бонусом отдам все аксессуары.`,
-  `Даю недельную гарантию.`,
-  `Если товар не понравится — верну всё до последней копейки.`,
-  `Это настоящая находка для коллекционера!`,
-  `Если найдёте дешевле — сброшу цену.`,
-  `Таких предложений больше нет!`,
-  `При покупке с меня бесплатная доставка в черте города.`,
-];
-
-const CATEGORIES = [`Книги`, `Разное`, `Посуда`, `Игры`, `Животные`, `Журналы`];
 
 const OfferType = {
   OFFER: `offer`,
@@ -57,9 +35,7 @@ module.exports = {
   DEFAULT_COUNT,
   MAX_DESCRIPTION_LENGTH,
   FILE_NAME,
-  TITLES,
-  SENTENCES,
-  CATEGORIES,
+  DATA_DIR,
   OfferType,
   SumRestrict,
   PictureRestrict,
--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -3,36 +3,50 @@
 const fs = require(`fs`).promises;
 const chalk = require(`chalk`);
 
-const {DEFAULT_COUNT, CATEGORIES, SENTENCES, TITLES, OfferType, SumRestrict, PictureRestrict, PICTURE_NAME, FILE_NAME, MAX_DESCRIPTION_LENGTH, ExitCodes} = require(`../../constants`);
+const {DEFAULT_COUNT, OfferType, DATA_DIR, SumRestrict, PictureRestrict, PICTURE_NAME, FILE_NAME, MAX_DESCRIPTION_LENGTH, ExitCodes} = require(`../../constants`);
 const {
   getRandomInt,
   shuffle,
   getRandomItem
 } = require(`../../utils`);
 
+const readFromFile = async (fileName) => {
+  try {
+    const data = await fs.readFile(fileName, {encoding: `utf-8`});
+    return data.trim().split(`\n`);
+  } catch (e) {
+    console.error(chalk.red(`Can't read file ${fileName}: ${e}`));
+    return [];
+  }
+};
+
 const getPictureFileName = (number) => {
   const replaceBy = number.toString().length === 1 ? `0${number}` : `${number}`;
   return PICTURE_NAME.replace(/XX/, replaceBy);
 };
 
+const generateOffers = async (count) => {
+  const categories = await readFromFile(`${DATA_DIR}/categories.txt`);
+  const sentences = await readFromFile(`${DATA_DIR}/sentences.txt`);
+  const titles = await readFromFile(`${DATA_DIR}/titles.txt`);
 
-const generateOffers = (count) => (
-  Array(count).fill({}).map(() => ({
-    category: shuffle(CATEGORIES).slice(0, getRandomInt(1, CATEGORIES.length)),
-    description: shuffle(SENTENCES).slice(0, getRandomInt(1, MAX_DESCRIPTION_LENGTH)).join(` `),
+  return Array(count).fill({}).map(() => ({
+    category: shuffle(categories).slice(0, getRandomInt(1, categories.length)),
+    description: shuffle(sentences).slice(0, getRandomInt(1, MAX_DESCRIPTION_LENGTH)).join(` `),
     picture: getPictureFileName(getRandomInt(PictureRestrict.MIN, PictureRestrict.MAX)),
-    title: getRandomItem(TITLES),
+    title: getRandomItem(titles),
     type: getRandomItem(Object.values(OfferType)),
     sum: getRandomInt(SumRestrict.MIN, SumRestrict.MAX),
-  }))
-);
+  }));
+};
 
 module.exports = {
   name: `--generate`,
   run: async (args) => {
     const [count] = args;
     const offerCount = parseInt(count, 10) || DEFAULT_COUNT;
-    const content = JSON.stringify(generateOffers(offerCount));
+    const generatedOffers = await generateOffers(offerCount);
+    const content = JSON.stringify(generatedOffers);
 
     try {
       await fs.writeFile(FILE_NAME, content);
