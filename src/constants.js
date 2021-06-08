2cdaafe0d20fb7f2993689d38ccb46bac3b7de81
Dmitry Kitsenko <kitsenko@greensight.ru>
2021-06-08 22:58:31 +0300

    add server

--- src/constants.js
+++ src/constants.js
@@ -4,6 +4,7 @@ const USER_ARGV_INDEX = 2;
 const DEFAULT_USER_COMAND = `--help`;
 const DEFAULT_COUNT = 1;
 const MAX_DESCRIPTION_LENGTH = 5;
+const DEFAULT_PORT = 3000;
 
 const FILE_NAME = `mocks.json`;
 
@@ -32,6 +33,7 @@ module.exports = {
   USER_ARGV_INDEX,
   DEFAULT_USER_COMAND,
   DEFAULT_COUNT,
+  DEFAULT_PORT,
   MAX_DESCRIPTION_LENGTH,
   FILE_NAME,
   OfferType,
@@ -39,4 +41,12 @@ module.exports = {
   PictureRestrict,
   PICTURE_NAME,
   ExitCodes
+};
+
+module.exports.HttpCode = {
+  OK: 200,
+  NOT_FOUND: 404,
+  INTERNAL_SERVER_ERROR: 500,
+  FORBIDDEN: 403,
+  UNAUTHORIZED: 401,
 };
