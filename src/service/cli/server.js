a37a7d14b71acf9d60cb0ad2bba1ae5953a40c67
Dmitry Kitsenko <kitsenko@greensight.ru>
2021-06-15 22:33:14 +0300

    server refactoring

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -1,57 +1,33 @@
 "use strict";
 
 const chalk = require(`chalk`);
-const http = require(`http`);
+const express = require(`express`);
 const fs = require(`fs`).promises;
-const {DEFAULT_PORT, FILE_NAME, HttpCode} = require(`../../constants`);
+const {DEFAULT_PORT, FILE_NAME} = require(`../../constants`);
 
-const sendResponse = (res, statusCode, message) => {
-  const template = `
-    <!Doctype html>
-      <html lang="ru">
-      <head>
-        <title>With love from Node</title>
-      </head>
-      <body>${message}</body>
-    </html>`.trim();
-
-  res.writeHead(statusCode, {'Content-Type': `text/html; charset=UTF-8`});
-  res.end(template);
-};
-
-const onClientConnect = async (req, res) => {
-  const notFoundMessageText = `Not found`;
-
-  switch (req.url) {
-    case `/`:
-      try {
-        const fileContent = await fs.readFile(FILE_NAME);
-        const mocks = JSON.parse(fileContent);
-        const posts = mocks.map((post) => `<li>${post.title}</li>`).join(``);
-        sendResponse(res, HttpCode.OK, `<ul>${posts}</ul>`);
-      } catch (e) {
-        sendResponse(res, HttpCode.NOT_FOUND, notFoundMessageText);
-      }
-      break;
-    default: {
-      sendResponse(res, HttpCode.NOT_FOUND, notFoundMessageText);
-      break;
-    }
+const offersRouter = new express.Router();
+offersRouter.get(`/`, async (req, res) => {
+  try {
+    const fileContent = await fs.readFile(FILE_NAME);
+    const mocks = JSON.parse(fileContent);
+    res.json(mocks);
+  } catch (err) {
+    res.json([]);
   }
-};
+});
 
 module.exports = {
   name: `--server`,
   run: (args) => {
     const [customPort] = args;
     const port = parseInt(customPort, 10) || DEFAULT_PORT;
-    http.createServer(onClientConnect)
-      .listen(port)
-      .on(`listening`, () => {
-        console.info(chalk.green(`Server listening on port: ${port}`));
-      })
-      .on(`error`, (err) => {
-        console.error(chalk.red(`Creation server error: ${err}`));
-      });
+
+    const app = express();
+    app.use(express.json());
+    app.use(`/offers`, offersRouter);
+
+    app.listen(port, () => {
+      console.info(chalk.green(`Server listening on port ${port}: http://localhost:${port}`));
+    });
   },
 };
