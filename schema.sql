3a6e71d987b9ea2f64c853eb27ea8beb62003d00
Dmitry Kitsenko <kitsenko@greensight.ru>
2021-09-11 23:34:33 +0300

    fixes after review

--- schema.sql
+++ schema.sql
@@ -9,7 +9,7 @@ CREATE TABLE users(
   password_hash varchar(255) NOT NULL,
   first_name varchar(255) NOT NULL,
   last_name varchar(255) NOT NULL,
-  avatar varchar(50) NOT NULL,
+  avatar varchar(50) NOT NULL
 );
 
 CREATE TABLE offers(
@@ -18,26 +18,28 @@ CREATE TABLE offers(
   description text NOT NULL,
   sum integer NOT NULL,
   type varchar(5) NOT NULL,
-  picture varchar(50) NOT NULL,
-  created_at timestamp DEFAULT current_timestamp,
+  picture varchar(50),
   user_id integer NOT NULL,
+  created_at timestamp DEFAULT current_timestamp,
   FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
-CREATE INDEX ON offers (title);
-
 CREATE TABLE comments(
   id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  offer_id integer NOT NULL,
+  user_id integer NOT NULL,
   text text NOT NULL,
-  created_at timestamp DEFAULT current_timestamp
-  FOREIGN KEY (user_id) REFERENCES user(id),
-  FOREIGN KEY (offer_id) REFERENCES offers(id),
+  created_at timestamp DEFAULT current_timestamp,
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (offer_id) REFERENCES offers(id)
 );
 
 CREATE TABLE offer_categories(
   offer_id integer NOT NULL,
-  category_id integer NOT NULL
-  PRIMARY KEY (offer_id, category_id) GENERATED ALWAYS AS IDENTITY,
+  category_id integer NOT NULL,
+  PRIMARY KEY (offer_id, category_id),
   FOREIGN KEY (offer_id) REFERENCES offers(id),
-  FOREIGN KEY (category_id) REFERENCES categories(id),
-)
+  FOREIGN KEY (category_id) REFERENCES categories(id)
+);
+
+CREATE INDEX ON offers(title);
